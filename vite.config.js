46f3c831364cb8034201d10c48a7c836595f16ab
Chandrukr <cshekar4244@gmail.com>
2025-02-21 08:32:40 +0530

    test: adding port 3000 config in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: './',
     css: {
       devSourcemap: false
-    }
+    },
+    server: {
+    port: 3000,  // Set the port to 3000
+    host: true,   // Allow external access (0.0.0.0)
+    strictPort: true, // the server fails if the port is already in use
+    allowedHosts: [
+      '.amazonaws.com' // allow all AWS instances
+    ]
+  },
 })
